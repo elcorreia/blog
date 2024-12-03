3ebbe2c926685c2f933c292967d80447b3f8f805
Erik Lopes <elcorreiapro@gmail.com>
2024-12-02 22:10:35 -0300

    author

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthorsResolver } from './graphql/resolvers/authors.resolver';
 import { DatabaseModule } from '@/database/database.module';
+import { PrismaService } from '@/database/prisma/prisma.service';
+import { AuthorPrismaRepository } from './repositories/authors-prisma.repository';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [AuthorsResolver]
+  providers: [
+    AuthorsResolver,
+    {
+      provide : 'PrismaService',
+      useClass: PrismaService
+    },
+    {
+      provide: 'AuthorRespository',
+      useFactory: (prisma: PrismaService) => {
+        return new AuthorPrismaRepository(prisma)
+      },
+      inject : ['PrismaService']
+    }
+  ]
 })
 export class AuthorsModule {}
---
+++ src/authors/repositories/authors-prisma.repository.ts
@@ -0,0 +1,48 @@
+import { PrismaService } from "@/database/prisma/prisma.service";
+import { Author } from "../graphql/models/author";
+import { IAuthorsRepository, SearchParams, SearchResult } from "../interfaces/authors.repository";
+import { ICreateAuthor } from "../interfaces/create-author";
+import { NotFoundError } from "@/shared/erros/not-found-error";
+
+export class AuthorPrismaRepository implements IAuthorsRepository{
+  sortableFields: string[] = ['name', 'email', 'createdAt']
+
+  constructor(private prisma: PrismaService){}
+
+  create(data: ICreateAuthor): Promise<Author> {
+    throw new Error("Method not implemented.");
+  }
+
+  update(author: Author): Promise<Author> {
+    throw new Error("Method not implemented.");
+  }
+
+  delete(id: string): Promise<Author> {
+    throw new Error("Method not implemented.");
+  }
+
+  async findById(id: string): Promise<Author> {
+    return await this.get(id);
+  }
+
+  findByEmail(email: string): Promise<Author> {
+    throw new Error("Method not implemented.");
+  }
+
+  search(params: SearchParams): Promise<SearchResult> {
+    throw new Error("Method not implemented.");
+  }
+
+  async get(id: string): Promise<Author> {
+
+    const author = await this.prisma.author.findUnique({
+      where: { id}
+    })
+
+    if(!id){
+      throw new NotFoundError(`Author not found using ID ${id}`);
+    }
+
+    return author
+  }
+}
