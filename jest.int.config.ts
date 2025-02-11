74aeab6f18481043acdd2f0ffb756faa5a519c43
Erik Lopes <elcorreiapro@gmail.com>
2025-02-10 21:26:26 -0300

    Implementando repository de authors e testes

--- jest.int.config.ts
+++ jest.int.config.ts
@@ -12,4 +12,5 @@ export default {
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
+  testTimeout: 90000,
 }
--- src/authors/repositories/authors-prisma-repository.int-spec.ts
+++ src/authors/repositories/authors-prisma-repository.int-spec.ts
@@ -73,7 +73,85 @@ describe('AuthorsPrismaRepository Integrations Tests', () => {
       result.items.forEach(item => {
         expect(item.id).toBeDefined()
       })
+
+      result.items.reverse().forEach((item, index) => {
+        expect(`${item.email}${index + 1}@a.com`)
+      })
     })
+
+
+    test('should apply pagination and ordering', async () => {
+      const createdAt = new Date()
+      const data = []
+      const arrange = 'badec'
+      arrange.split('').forEach((element, index) => {
+        const timestamp = createdAt.getTime() + index
+        data.push({
+          ...AuthorDataBuilder({ name: element }),
+          email: `author${index}@a.com`,
+          createdAt: new Date(timestamp),
+        })
+      })
+
+      await prisma.author.createMany({ data })
+      const result1 = await repository.search({
+        page: 1,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+      })
+
+      expect(result1.items[0]).toMatchObject(data[1])
+      expect(result1.items[1]).toMatchObject(data[0])
+
+      const result2 = await repository.search({
+        page: 2,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+      })
+
+      expect(result2.items[0]).toMatchObject(data[4])
+      expect(result2.items[1]).toMatchObject(data[2])
+    })
+
+    test('should apply pagination, filter and ordering', async () => {
+      const createdAt = new Date()
+      const data = []
+      const arrange = ['test', 'a', 'TEST', 'b', 'Test']
+      arrange.forEach((element, index) => {
+        const timestamp = createdAt.getTime() + index
+        data.push({
+          ...AuthorDataBuilder({ name: element }),
+          email: `author${index}@a.com`,
+          createdAt: new Date(timestamp),
+        })
+      })
+
+      await prisma.author.createMany({ data })
+      const result1 = await repository.search({
+        page: 1,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'TEST',
+      })
+
+      expect(result1.items[0]).toMatchObject(data[0])
+      expect(result1.items[1]).toMatchObject(data[4])
+
+      const result2 = await repository.search({
+        page: 2,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'TEST',
+      })
+
+      expect(result2.items[0]).toMatchObject(data[2])
+      expect(result2.items.length).toBe(1)
+    })
+
   })
 
 });
