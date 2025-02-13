40cb6d0f6245e681f64c7a01728ee56b536ac545
Erik Lopes <elcorreiapro@gmail.com>
2025-02-12 21:20:31 -0300

    Implementando graphql no módulo de autores

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { AuthorsResolver } from './graphql/resolvers/authors.resolver';
 import { DatabaseModule } from '@/database/database.module';
 import { PrismaService } from '@/database/prisma/prisma.service';
-import { AuthorPrismaRepository } from './repositories/authors-prisma.repository';
-
+import { AuthorsPrismaRepository } from './repositories/authors-prisma.repository'
+import { ListAuthorsUsecase } from './usecases/list-authors.usecase'
 @Module({
   imports: [DatabaseModule],
   providers: [
@@ -13,12 +13,19 @@ import { AuthorPrismaRepository } from './repositories/authors-prisma.repository
       useClass: PrismaService
     },
     {
-      provide: 'AuthorRespository',
+      provide: 'AuthorsRepository',
       useFactory: (prisma: PrismaService) => {
-        return new AuthorPrismaRepository(prisma)
+        return new AuthorsPrismaRepository(prisma)
       },
       inject : ['PrismaService']
-    }
+    },
+    {
+      provide: ListAuthorsUsecase.Usecase,
+      useFactory: (authosRepository: AuthorsPrismaRepository) => {
+        return new ListAuthorsUsecase.Usecase(authosRepository)
+      },
+      inject: ['AuthorsRepository'],
+    },
   ]
 })
 export class AuthorsModule {}
--- src/authors/graphql/resolvers/authors.resolver.ts
+++ src/authors/graphql/resolvers/authors.resolver.ts
@@ -1,13 +1,27 @@
-import { PrismaService } from '@/database/prisma/prisma.service';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql'
 import { Author } from '../models/author';
+import { Inject } from '@nestjs/common'
+import { ListAuthorsUsecase } from '@/authors/usecases/list-authors.usecase'
+import { SearchParamsArgs } from '../args/search-params.args'
+import { SearchAuthorsResult } from '../models/search-authors-result'
 
 @Resolver(() => Author)
 export class AuthorsResolver {
-  constructor(private prisma: PrismaService ){}
 
-  @Query(()=> [Author])
-  authors(){
-    return this.prisma.author.findMany()
+  @Inject(ListAuthorsUsecase.Usecase)
+  private listAuthorUseCase: ListAuthorsUsecase.Usecase
+
+  @Query(() => SearchAuthorsResult)
+  async authors(
+    @Args() { page, perPage, sort, sortDir, filter }: SearchParamsArgs,
+  ) {
+    const list = await this.listAuthorUseCase.execute({
+      page,
+      perPage,
+      sort,
+      sortDir,
+      filter,
+    })
+    return list
   }
 }
