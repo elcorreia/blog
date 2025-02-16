01805bafc5e3bd9f82568bbdb58d21eae89f1bfd
Erik Lopes <elcorreiapro@gmail.com>
2025-02-16 01:07:36 -0300

    Implementando graphql no modulo de posts

--- src/posts/graphql/resolvers/posts.resolver.ts
+++ src/posts/graphql/resolvers/posts.resolver.ts
@@ -1,9 +1,13 @@
-import { Args, Mutation, Parent, ResolveField, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql'
 import { Post } from '../models/post'
 import { Inject } from '@nestjs/common'
 import { CreatePostUseCase } from '@/posts/usecases/create-post.usecase'
 import { CreatePostInput } from '../inputs/create-post.input'
 import { GetAuthorUsecase } from '@/authors/usecases/get-author.usecase'
+import { GetPostUseCase } from '@/posts/usecases/get-post.usecase'
+import { PostIdArgs } from '@/posts/graphql/args/post-id.args'
+import { PublishPostUseCase } from '@/posts/usecases/publish-post.usecase'
+import { UnpublishPostUseCase } from '@/posts/usecases/unpublish-post.usecase'
 
 @Resolver(() => Post)
 export class PostsResolver {
@@ -12,6 +16,22 @@ export class PostsResolver {
 
   @Inject(GetAuthorUsecase.Usecase)
   private getAuthorUseCase: GetAuthorUsecase.Usecase
+
+  @Inject(GetPostUseCase.UseCase)
+  private getPostUseCase: GetPostUseCase.UseCase
+
+  @Inject(PublishPostUseCase.UseCase)
+  private publishPostUseCase: PublishPostUseCase.UseCase
+
+
+  @Inject(UnpublishPostUseCase.UseCase)
+  private unpublishPostUseCase: UnpublishPostUseCase.UseCase
+
+
+  @Query(() => Post)
+  async getPostById(@Args() { id }: PostIdArgs) {
+    return this.getPostUseCase.execute({ id })
+  }
 
 
   @Mutation(() => Post)
@@ -22,5 +42,15 @@ export class PostsResolver {
   @ResolveField()
   author(@Parent() post: Post) {
     return this.getAuthorUseCase.execute({ id: post.authorId })
+  }
+
+  @Mutation(() => Post)
+  async publishPost(@Args() { id }: PostIdArgs) {
+    return this.publishPostUseCase.execute({ id })
+  }
+
+  @Mutation(() => Post)
+  async unpublishPost(@Args() { id }: PostIdArgs) {
+    return this.unpublishPostUseCase.execute({ id })
   }
 }
